d70e4084519fd44829b8ae4d53ddb7787ed089e7
victor-renan <alvesrenanpro@gmail.com>
2023-06-19 01:18:27 -0300

    feat(): requirements and vercel json

--- facerecogpyocv/settings.py
+++ facerecogpyocv/settings.py
@@ -85,6 +85,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static_media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
